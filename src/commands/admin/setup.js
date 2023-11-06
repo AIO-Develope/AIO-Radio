351b2b2c2f57c7a0f79e94423317e51665041fc0
AIO <69240351+AIO-Develope@users.noreply.github.com>
2023-11-06 18:00:26 +0100

    basic setup command added
    /setup [channel]
    
    - validating channel for vc
    - reply

---
+++ src/commands/admin/setup.js
@@ -0,0 +1,36 @@
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js')
+
+
+module.exports = {
+    name: 'setup',
+    description: 'Set the voice channel you want to use for the radio.',
+    // devOnly:
+    // testOnly:
+    options: [
+        {
+            name: 'channel',
+            description: 'The channel you want to join the bot.',
+            require: true,
+            type: ApplicationCommandOptionType.Channel,
+        }
+
+
+],
+
+permissionsRequired: [PermissionFlagsBits.Administrator],
+
+
+    callback: (client, interaction) =>{
+
+        const channelObject = interaction.options.getChannel('channel');
+
+
+        if (channelObject.type === 2){
+            interaction.reply(`Bot is now set to channel ${channelObject.name}`)
+
+        }else{
+            interaction.reply(`${channelObject.name} is not a voice channel!`)
+
+        }
+    },
+};
